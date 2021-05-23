512ac87aded7df183b8ae054e79c2432b0f05b44
yotam91 <52106121+yotam91@users.noreply.github.com>
2021-05-23 20:29:26 +0300

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -1,49 +1,38 @@
 import hashlib
 
-import random
-
-
-def countTrailingZero(x):
-    count = 0
-    while ((x & 1) == 0):
-        x = x >> 1
-        count += 1
-
-    return count
+#import random
 
 
 def hash_preimage(target_string):
     result = 0
-    if not all([x in '01' for x in target_string]):
-        print("Input should be a string of bits")
+    if not all( [x in '01' for x in target_string ] ):
+        print( "Input should be a string of bits" )
         nonce = b'\x00'
 
-    target_string = int(target_string)
-
-    for i in range(100):
-        xInt = random.randint(1, 100)
-        x = xInt.to_bytes(2, 'big')
-        xHash256 = hashlib.sha256(x)
-        xHash256 = xHash256.hexdigest()
-        xBits = ''.join(format(ord(i), '08b') for i in xHash256)
-
-        if countTrailingZero(xInt) == countTrailingZero(target_string):
-            nonce = xInt
-
-    if nonce != b'\x00':
-        return (nonce)
-    else:
-        return ('not found')
+    intTarget_string = int(target_string)
+    i = 0
+    while True:
+        x = i
+        xBytes = bytes(x)
+        h_hex = hashlib.sha256( xBytes ).hexdigest()
+        
+        #convert to binary
+        h_bin = bin( int( h_hex, base=16 ) )[2:]
+        end = len(h_bin)
+        extractedBits = h_bin[end-len(target_string): end]
 
 
-# Here the '10010' is the users input
-
-print(hash_preimage('10010'))
-
-# Use the following lines if you want to get input from the users on the command line
-
-# x = input('Enter a binary value ')
-# print(hash_preimage(str(x)))
+        if extractedBits==target_string:
+            
+            return(bytes(x))
+        else:
+            #print('Retrying')
+            i = i+1
 
 
+# Enter any binary value here
+target_hash = '100'
+
+
+print(hash_preimage(target_hash))
 
